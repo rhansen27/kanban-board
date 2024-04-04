6af29da30199cec7adbd239ab0628c0841acc570
rph <rhansen0127@gmail.com>
2024-04-04 12:40:04 -0500

    added getter and setter functions for local storage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,38 +1,51 @@
-// Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+// declare variables
+const $swimLane = $(".swimlane");
+const $taskList = $("#task-list");
+
+// create a function to get localStorage data
+function getLocalStorageData() {
+  // get tasks and nextId from localStorage
+  // if there is no data in localStorage, set tasks and nextId to empty arrays
+  const taskList = JSON.parse(localStorage.getItem("tasks") || []);
+  const taskID = JSON.parse(localStorage.getItem("nextId") || []);
+
+  return taskList;
+  return nextId;
+}
+// create a function to set localStorage data
+function setLocalStorageData(taskList, nextId) {
+  // set tasks and nextId to localStorage
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+}
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
+function generateId() {
+  let newId = nextId.id;
+  nextId.id++;
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return newId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+  const card =
+    $(`<div class="card draggable" data-id="${nextId.id} data-status=${task.status}">
+  <div class="card-body">
+    <h5 class="card-title">${task.title}</h5>
+    <p class="card-text">${task.description}</p>
+    <p class="card-text">${task.dueDate}</p>
+    <button class="btn btn-danger delete">Delete</button>`);
+  return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
